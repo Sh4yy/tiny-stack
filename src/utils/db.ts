36ab93b8df2e688add327dc79bbd604e22749514
Shayan <ltaslim@montgomerycollege.edu>
2023-12-30 17:42:20 -0500

    wip

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,12 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
+import * as schema from '../models/schema.ts';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 const sqlite = new Database(
   import.meta.env.PROD ? '/data/db.sqlite3' : './db.sqlite3'
 );
-export const db = drizzle(sqlite);
+
+export const db = drizzle(sqlite, { schema });
 
 migrate(db, { migrationsFolder: './drizzle' });
