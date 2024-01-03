6fa4d0a7dc7d52685622cc019642bba1e29128b7
nurodev <ben@nuro.dev>
2024-01-03 17:08:39 +0000

    Added Drizzle adapter to Auth.js config

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,12 @@
 import GitHub from '@auth/core/providers/github';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+
+import { db } from './src/utils/db';
 
 import type { AuthConfig } from '@auth/core';
 
 export default {
+  adapter: DrizzleAdapter(db),
   providers: [
     GitHub({
       clientId: import.meta.env.GITHUB_CLIENT_ID,
